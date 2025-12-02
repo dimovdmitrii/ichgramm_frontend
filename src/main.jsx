5a1cd41a709dbcf3644353a37ef6fdec1e205f01
dimovdmitrii <dimovdima1509@gmail.com>
2025-12-03 00:43:12 +0100

    authSlice /api / Operations rootReducer

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { store, persistor } from "./store/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
